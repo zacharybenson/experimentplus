6a3a406277be2611b3f742224e9fe287da7393bc
Zachary Benson <49580727+zacharybenson@users.noreply.github.com>
2022-05-26 12:09:15 -0500

    Initial

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.urls import re_path, TemplateView
+from django.urls import re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
